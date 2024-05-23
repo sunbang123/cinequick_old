8685c6f43e2efbf61efb4529d03286000571e420
sunbang123 <sunbang123@users.noreply.github.com>
2024-05-23 05:34:20 +0000

    deploy: 7f3aee83ee517e983f53bef9f4fe89d1abd8028e

--- script.js
+++ script.js
@@ -1,40 +1,83 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const fetchDataButton = document.getElementById('fetch-data');
-    fetchDataButton.addEventListener('click', fetchBoxOfficeData);
-  
-    async function fetchBoxOfficeData() {
-      const dateInput = document.getElementById('date').value;
-      const url = `/api/boxoffice?targetDt=${dateInput}`;
-  
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        displayBoxOfficeData(data);
-      } catch (error) {
-        console.error('Error fetching box office data:', error);
+  const fetchDataButton = document.getElementById('fetch-data');
+  fetchDataButton.addEventListener('click', fetchBoxOfficeData);
+
+  async function fetchBoxOfficeData() {
+    const dateInput = document.getElementById('date').value;
+    const apiUrl = window.location.hostname === 'localhost'
+      ? `http://localhost:5000/api/boxoffice?targetDt=${dateInput}`
+      : `https://sunbang123.github.io/cinequick/api/boxoffice?targetDt=${dateInput}`;  // 실제 배포된 백엔드 도메인을 여기에 입력합니다.
+
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
+      const data = await response.json();
+      displayBoxOfficeData(data);
+    } catch (error) {
+      console.error('Error fetching box office data:', error);
     }
+  }
+
+  function displayBoxOfficeData(data) {
+    const boxOfficeResults = document.getElementById('boxoffice-results');
+    boxOfficeResults.innerHTML = '';
+
+    const boxOfficeList = data.boxOfficeResult.dailyBoxOfficeList;
+
+    boxOfficeList.forEach(movie => {
+      const movieItem = document.createElement('div');
+      movieItem.classList.add('movie-item');
+      movieItem.innerHTML = `
+        <h2>${movie.movieNm}</h2>
+        <p>Rank: ${movie.rank}</p>
+        <p>Sales Amount: ${movie.salesAmt}</p>
+        <p>Audience Count: ${movie.audiCnt}</p>
+      `;
+      boxOfficeResults.appendChild(movieItem);
+    });
+  }
+});
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const fetchDataButton = document.getElementById('fetch-data');
+//     fetchDataButton.addEventListener('click', fetchBoxOfficeData);
   
-    function displayBoxOfficeData(data) {
-      const boxOfficeResults = document.getElementById('boxoffice-results');
-      boxOfficeResults.innerHTML = '';
+//     async function fetchBoxOfficeData() {
+//       const dateInput = document.getElementById('date').value;
+//       const url = `/api/boxoffice?targetDt=${dateInput}`;
   
-      const boxOfficeList = data.boxOfficeResult.dailyBoxOfficeList;
+//       try {
+//         const response = await fetch(url);
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         const data = await response.json();
+//         displayBoxOfficeData(data);
+//       } catch (error) {
+//         console.error('Error fetching box office data:', error);
+//       }
+//     }
   
-      boxOfficeList.forEach(movie => {
-        const movieItem = document.createElement('div');
-        movieItem.classList.add('movie-item');
-        movieItem.innerHTML = `
-          <h2>${movie.movieNm}</h2>
-          <p>Rank: ${movie.rank}</p>
-          <p>Sales Amount: ${movie.salesAmt}</p>
-          <p>Audience Count: ${movie.audiCnt}</p>
-        `;
-        boxOfficeResults.appendChild(movieItem);
-      });
-    }
-  });
+//     function displayBoxOfficeData(data) {
+//       const boxOfficeResults = document.getElementById('boxoffice-results');
+//       boxOfficeResults.innerHTML = '';
+  
+//       const boxOfficeList = data.boxOfficeResult.dailyBoxOfficeList;
+  
+//       boxOfficeList.forEach(movie => {
+//         const movieItem = document.createElement('div');
+//         movieItem.classList.add('movie-item');
+//         movieItem.innerHTML = `
+//           <h2>${movie.movieNm}</h2>
+//           <p>Rank: ${movie.rank}</p>
+//           <p>Sales Amount: ${movie.salesAmt}</p>
+//           <p>Audience Count: ${movie.audiCnt}</p>
+//         `;
+//         boxOfficeResults.appendChild(movieItem);
+//       });
+//     }
+//   });
   
